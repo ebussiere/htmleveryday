a87abbdbe5f4ea95433173150195b44f08aafb44
Eddy <eddy@embtech.ca>
2020-12-11 17:17:22 -0700

    Initial

---
+++ 01_binarytodecimal/index.js
@@ -0,0 +1,18 @@
+const binary = document.querySelector('#binary');
+const decimal = document.querySelector('#decimal');
+const btn = document.querySelector('#cbtn').addEventListener('click', init);
+const invalidMessage = "Enter a valid binary number to convert.";
+decimal.value = invalidMessage;
+function init(e) {
+  binary.addEventListener('input', runevent);
+  runevent(e);
+};
+
+function runevent(e) {
+  if (parseInt(binary.value, 2)) {
+    decimal.value = parseInt(binary.value, 2);
+  }
+  else {
+    decimal.value = invalidMessage;
+  }
+}
